1ddd3fecc4c6c2598f73562b44bcea7fb7466afe
BearNov <153312154+BearNov@users.noreply.github.com>
2026-07-10 14:06:25 +0300

    Add Python current auth user test

--- 05-python-automation/tests/test_auth_api.py
+++ 05-python-automation/tests/test_auth_api.py
@@ -30,3 +30,43 @@ def test_login_user():
     assert data["email"] != ""
     assert data["firstName"] != ""
     assert data["lastName"] != ""
+
+
+def test_get_current_auth_user():
+    login_payload = {
+        "username": "emilys",
+        "password": "emilyspass",
+        "expiresInMins": 30
+    }
+
+    login_response = requests.post(f"{BASE_URL}/auth/login", json=login_payload)
+
+    assert login_response.status_code == 200
+
+    login_data = login_response.json()
+
+    assert "accessToken" in login_data
+
+    access_token = login_data["accessToken"]
+
+    headers = {
+        "Authorization": f"Bearer {access_token}"
+    }
+
+    response = requests.get(f"{BASE_URL}/auth/me", headers=headers)
+
+    assert response.status_code == 200
+
+    user = response.json()
+
+    assert user["id"] == login_data["id"]
+    assert user["username"] == login_data["username"]
+    assert user["email"] == login_data["email"]
+
+    assert "firstName" in user
+    assert "lastName" in user
+    assert "role" in user
+
+    assert user["firstName"] != ""
+    assert user["lastName"] != ""
+    assert user["role"] != ""
